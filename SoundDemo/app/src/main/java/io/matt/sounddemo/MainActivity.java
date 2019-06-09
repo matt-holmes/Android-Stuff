02159fad8d9bd2a34ecc796550ec71e7551ed26d
matt-holmes <matt.m.holmes@protonmail.com>
2019-06-09 18:32:21 -0500

    adding a volume and location slider

--- SoundDemo/app/src/main/java/io/matt/sounddemo/MainActivity.java
+++ SoundDemo/app/src/main/java/io/matt/sounddemo/MainActivity.java
@@ -1,20 +1,71 @@
 package io.matt.sounddemo;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.SeekBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer myMediaPlayer;
+    AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myMediaPlayer = MediaPlayer.create(this, R.raw.barking );
+        SeekBar volumeControl = (SeekBar) findViewById(R.id.seekBar);
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int curVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        volumeControl.setMax(maxVolume);
+        volumeControl.setProgress(curVolume);
+        volumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        SeekBar audioLocation = (SeekBar) findViewById(R.id.audioLocation);
+        audioLocation.setMax(myMediaPlayer.getDuration());
+        audioLocation.setProgress(myMediaPlayer.getCurrentPosition());
+
+        audioLocation.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                myMediaPlayer.seekTo(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     public void buttonControls(View view) {
